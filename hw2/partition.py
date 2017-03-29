fea804158832bd6582ba5c335f497f952aa16c1c
kevin71104 <b03901026@ntu.edu.tw>
2017-03-29 23:53:11 +0800

    partition validation set

--- hw2/partition.py
+++ hw2/partition.py
@@ -18,37 +18,125 @@ train = []
 temp = 0
 with open(sys.argv[1],'r') as csvFile:
     for row in csv.reader(csvFile):
-        if temp == 0:
-            title = row
-            temp = 1
-        train.append( list( map(myint,row) ) )
-# retrieve first row
-train  = np.array(train[1:])
-person = train.shape[0]  # number of person
-num_w  = train.shape[1]  # number of weight
+        train.append(row)
+title = train[0]
+train = train[1:]
 
 ####################### write data ########################
-div = int(person/3)
-part = train[:(div+1)]
-with open(sys.argv[2],'w') as csvFile:
+div = int (len(train)/3)
+#print(div)
+part1 = train[:div]
+part2 = train[div:div*2]
+part3 = train[div*2:]
+with open('x1.csv','w') as csvFile:
     for i in range(len(title)):
         if i == 0:
             csvFile.write(title[i])
         else:
             csvFile.write(',' + title[i])
-    for row in part:
+    for row in part1:
         csvFile.write('\n')
         for i in range(len(row)):
             if i == 0:
-                csvFile.write(str(row[i]))
+                csvFile.write(row[i])
             else:
-                csvFile.write(',' + str(row[i]) )
+                csvFile.write(',' + row[i])
+with open('x2.csv','w') as csvFile:
+    for i in range(len(title)):
+        if i == 0:
+            csvFile.write(title[i])
+        else:
+            csvFile.write(',' + title[i])
+    for row in part2:
+        csvFile.write('\n')
+        for i in range(len(row)):
+            if i == 0:
+                csvFile.write(row[i])
+            else:
+                csvFile.write(',' + row[i])
+with open('x3.csv','w') as csvFile:
+    for i in range(len(title)):
+        if i == 0:
+            csvFile.write(title[i])
+        else:
+            csvFile.write(',' + title[i])
+    for row in part3:
+        csvFile.write('\n')
+        for i in range(len(row)):
+            if i == 0:
+                csvFile.write(row[i])
+            else:
+                csvFile.write(',' + row[i])
+temp = part1 + part2
+with open('x12.csv','w') as csvFile:
+    for i in range(len(title)):
+        if i == 0:
+            csvFile.write(title[i])
+        else:
+            csvFile.write(',' + title[i])
+    for row in temp:
+        csvFile.write('\n')
+        for i in range(len(row)):
+            if i == 0:
+                csvFile.write(row[i])
+            else:
+                csvFile.write(',' + row[i])
+temp = part1 + part3
+with open('x13.csv','w') as csvFile:
+    for i in range(len(title)):
+        if i == 0:
+            csvFile.write(title[i])
+        else:
+            csvFile.write(',' + title[i])
+    for row in temp:
+        csvFile.write('\n')
+        for i in range(len(row)):
+            if i == 0:
+                csvFile.write(row[i])
+            else:
+                csvFile.write(',' + row[i])
+temp = part2 + part3
+with open('x23.csv','w') as csvFile:
+    for i in range(len(title)):
+        if i == 0:
+            csvFile.write(title[i])
+        else:
+            csvFile.write(',' + title[i])
+    for row in temp:
+        csvFile.write('\n')
+        for i in range(len(row)):
+            if i == 0:
+                csvFile.write(row[i])
+            else:
+                csvFile.write(',' + row[i])
 
 ####################### Read Y_train ########################
 label = []
 with open(sys.argv[2],'r') as csvFile:
     for row in csv.reader(csvFile):
-        #print(row)
-        label.extend( list( map(myint,row) ) )
-#print(label)
-label = np.array(label)
+        label.extend(row)
+####################### write Y_train #######################
+part1 = label[:div]
+part2 = label[div:div*2]
+part3 = label[div*2:]
+
+with open('y1.csv','w') as csvFile:
+    for i in range(len(part1)):
+        if i == 0:
+            csvFile.write(part1[i])
+        else:
+            csvFile.write('\n' + part1[i])
+
+with open('y2.csv','w') as csvFile:
+    for i in range(len(part2)):
+        if i == 0:
+            csvFile.write(part2[i])
+        else:
+            csvFile.write('\n' + part2[i])
+
+with open('y3.csv','w') as csvFile:
+    for i in range(len(part3)):
+        if i == 0:
+            csvFile.write(part3[i])
+        else:
+            csvFile.write('\n' + part3[i])
