93ae1426898c7126c57961e213ba2e3d1c0b50c8
kevin71104 <b03901026@ntu.edu.tw>
2017-04-19 19:19:28 +0800

    equal strong baseline

--- hw3/hw3_CNN.py
+++ hw3/hw3_CNN.py
@@ -2,6 +2,8 @@
 #                   Machine Learning 2017                   #
 #            Hw3 : Image Sentiment Classification           #
 #                Convolutional Neural Network               #
+#                Description : training model               #
+#            script : python3 train.csv model.h5            #
 #############################################################
 
 import pandas as pd
@@ -18,16 +20,17 @@ from keras.preprocessing.image import ImageDataGenerator
 ######################### Read File #########################
 with open(sys.argv[1],'r') as csvFile:
     train = pd.read_csv(csvFile)
-#train_feature = train['feature'].str.split(' ')
-#train_feature = train_feature.tolist()
 train_feature = train['feature']
+train_label   = np.array(train['label'])
+train = []
+
 x = []
 for i in range(train_feature.shape[0]):
     x.append(train_feature[i].split(' '))
 train_feature = np.array(x, dtype=float)
-train_feature = train_feature/255
+x =[]
 
-train_label   = np.array(train['label'])
+train_feature = train_feature/255
 classNum = 7
 
 #validation Data
@@ -36,6 +39,9 @@ valid_label = train_label[ :validNum]
 valid_feature = train_feature[ :validNum]
 x_label = train_label[validNum:]
 x_feature = train_feature[validNum: ]
+
+train_label = []
+train_feature = []
 
 x_feature = x_feature.reshape(x_feature.shape[0],48,48,1)
 valid_feature = valid_feature.reshape(valid_feature.shape[0],48,48,1)
@@ -46,26 +52,30 @@ valid_label = np_utils.to_categorical(valid_label, classNum)
 train_feature = train_feature.reshape(train_feature.shape[0],48,48,1)
 train_label = np_utils.to_categorical(train_label, classNum)
 """
+
 ######################### Start CNN #########################
 model = Sequential()
 
-model.add(Convolution2D(25,(3,3),input_shape=(48,48,1)))
-model.add(MaxPooling2D((2,2)))
-model.add(Convolution2D(50,(3,3)))
-model.add(MaxPooling2D((2,2)))
-model.add(Convolution2D(100,(3,3)))
-model.add(MaxPooling2D((2,2)))
+model.add(Convolution2D(32,(3,3), activation='relu', input_shape=(48,48,1)))
+#model.add(Convolution2D(32,(3,3), input_shape=(48,48,1)))
+model.add( MaxPooling2D(pool_size=(2, 2)) )
+model.add(Convolution2D(64,(3,3), activation='relu'))
+#model.add(Convolution2D(64,(3,3)))
+model.add( MaxPooling2D(pool_size=(2, 2)) )
+model.add(Convolution2D(128,(3,3), activation='relu'))
+#model.add(Convolution2D(128,(3,3)))
+model.add( MaxPooling2D(pool_size=(2, 2)) )
 
 model.add(Flatten())
 
-model.add(Dropout(0.3))
-model.add(Dense(512))
+model.add(Dropout(0.4))
+model.add(Dense(1024))
 model.add(Activation('relu'))
-model.add(Dropout(0.3))
-model.add(Dense(512))
+model.add(Dropout(0.4))
+model.add(Dense(1024))
 model.add(Activation('relu'))
 
-model.add(Dropout(0.3))
+model.add(Dropout(0.2))
 model.add(Dense(classNum))
 model.add(Activation('softmax'))
 
@@ -74,29 +84,30 @@ model.compile(loss='categorical_crossentropy', optimizer="adam", metrics=['accur
 
 # Image Preprocessing - add noise
 datagen = ImageDataGenerator(
-    rotation_range=2.0,
+    rotation_range=10.0,
     width_shift_range=0.1,
-    height_shift_range=0.1,
-    horizontal_flip = True,
-    vertical_flip = True,
+    height_shift_range=0.1
     )
 
-""" # No adding noise
-#es = EarlyStopping(monitor = 'val_loss', patience = 3, verbose = 1)
-#model.fit(train_feature, train_label, validation_split = 0.2, callbacks = [es], batch_size = 100, epochs = 20)
-#model.fit(train_feature, train_label, validation_split = 0.1, batch_size = 100, epochs = 20)
-"""
 # adding noise
-batchNum = 32
+batchNum = 100
 for i in range(4):
-    model.fit(x_feature, x_label,validation_data=(valid_feature,valid_label), batch_size = batchNum, epochs = 5)
+    model.fit(x_feature, x_label,validation_data=(valid_feature,valid_label), batch_size = batchNum, epochs = 4)
     model.fit_generator(datagen.flow(x_feature, x_label, batch_size = batchNum),   # every flow has batchNum figures
                         steps_per_epoch = x_feature.shape[0]/batchNum,
-                        epochs = 5,
+                        epochs = 6,
                         validation_data = (valid_feature, valid_label)
                         )
 
-score = model.evaluate(train_feature, train_label)
-print ("\nTrain accuracy:", score[1])
-
 model.save(sys.argv[2])
+
+score = model.evaluate(x_feature, x_label)
+print ("\nTrain accuracy:", score[1])
+score2 = model.evaluate(valid_feature, valid_label)
+print ("\nValid accuracy:", score2[1])
+
+""" # No adding noise
+es = EarlyStopping(monitor = 'val_loss', patience = 3, verbose = 1)
+model.fit(train_feature, train_label, validation_split = 0.2, callbacks = [es], batch_size = 100, epochs = 20)
+model.fit(train_feature, train_label, validation_split = 0.1, batch_size = 100, epochs = 20)
+"""
