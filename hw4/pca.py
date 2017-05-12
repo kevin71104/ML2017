35eef668543dceab1c60482317fc7da6d94ad630
kevin71104 <b03901026@ntu.edu.tw>
2017-05-12 16:34:47 +0800

    Q1 done

--- hw4/pca.py
+++ hw4/pca.py
@@ -5,6 +5,7 @@
 
 from PIL import Image as im
 import numpy as np
+from numpy.linalg import norm
 import matplotlib.pyplot as plt
 
 filepath = './faceExpressionDatabase/'
@@ -25,8 +26,8 @@ figure1 = plt.figure()
 ax = figure1.add_subplot(1,1,1)
 ax.imshow(meanface,cmap='gray')
 ax.set_axis_off()
-figure1.suptitle('average face')
-figure1.savefig('average_face')
+figure1.suptitle('Average face')
+figure1.savefig('average_face.png')
 ################################ eigenface #####################################
 data_ctr = data - mean
 u, s, v = np.linalg.svd(data_ctr,full_matrices=False)
@@ -36,5 +37,41 @@ for i in range(9):
     subface = eigenface.add_subplot(3,3,i+1)
     subface.set_axis_off()
     subface.imshow(face,cmap='gray')
-eigenface.suptitle('eigenface')
-eigenface.savefig('eigenface')
+eigenface.suptitle('Eigenfaces')
+eigenface.savefig('eigenface.png')
+################################### reconstruct ################################
+'''reducematrix = v[0:5,:]
+coefficient = np.dot(data_ctr,np.transpose(reducematrix)) # c_i,j = (data_i-mu) dot w_j
+recover = plt.figure(figsize=(20, 20))
+origin = plt.figure(figsize=(20, 20))
+for i in range(100):
+    print(i)
+    recoverdata = np.sum(coefficient[i,:].reshape(5,1) * reducematrix,axis=0) + mean
+    recoverface = recoverdata.reshape(64,64)
+    subface = recover.add_subplot(10,10,i+1)
+    subface.set_axis_off()
+    subface.imshow(recoverface,cmap='gray')
+
+    originface = data[i,:].reshape(64,64)
+    subface2 = origin.add_subplot(10,10,i+1)
+    subface2.set_axis_off()
+    subface2.imshow(originface,cmap='gray')
+
+recover.suptitle('Reconstruct',fontsize=36)
+recover.savefig('reconstruct.png')
+origin.suptitle('Original',fontsize=36)
+origin.savefig('original.png')'''
+
+for k in range(1,v.shape[0]+1):
+    RMSE = 0
+    reducematrix = v[:k,:]
+    coefficient = np.dot(data_ctr,np.transpose(reducematrix))
+    for i in range(100):
+        recoverdata = np.sum(coefficient[i,:].reshape(k,1) * reducematrix,axis=0) + mean
+        error = np.square(norm(data[i,:]-recoverdata))
+        RMSE = RMSE + error
+    RMSE = np.sqrt((RMSE / data.shape[0]) / data.shape[1]) / 255
+    print('RMSE of reduction dimension({}): {}'.format(k,RMSE))
+    if RMSE < 0.01:
+        print('At reduction dimension({}), RMSE < 1%'.format(k))
+        break
