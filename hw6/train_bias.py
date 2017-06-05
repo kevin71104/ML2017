339dec909b17b4f69fbebfb117133ede1c67d29e
kevin71104 <b03901026@ntu.edu.tw>
2017-06-05 23:27:22 +0800

    many model version

--- hw6/train_bias.py
+++ hw6/train_bias.py
@@ -5,6 +5,7 @@ from keras.layers import Input,Embedding, Reshape, Merge, Dropout,Flatten,Dot,Ad
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras import regularizers
 from argparse import ArgumentParser
+import pandas as pd
 
 def read_data(path,train):
     users = []
@@ -46,8 +47,11 @@ def split_data(U,M,R,split_ratio):
 parser = ArgumentParser()
 parser.add_argument('--dnn', action='store_true', help='Use DNN model')
 parser.add_argument('-n', '--normal', action='store_true', help='use Normalization')
+parser.add_argument('-e', '--extra', action='store_true', help='use extra feature')
 parser.add_argument('-m', '--model', help='model name')
 parser.add_argument('-o', '--output', help='output name')
+parser.add_argument('-d', '--dimension', help='latent dimension')
+parser.add_argument('-b', '--bias', action='store_true', help='use bias')
 args = parser.parse_args()
 
 # ==== Read Dataset ============================================================
@@ -68,10 +72,44 @@ max_movieid = np.max(movies)
 print("Find maximum user id {} and maximum movie id {} in training set..."\
 	.format(max_userid,max_movieid))
 
-embedding_dim = 10
-split_ratio = 0.1
-DROPOUT_RATE = 0.1
+if args.extra:
+    users_df = pd.read_csv('data/users.csv', sep='::', engine='python')
+    users_age = (users_df['Age'] - np.mean(users_df['Age'])) / np.std(users_df['Age'])
 
+    movies_df = pd.read_csv('data/movies.csv', sep='::', engine='python')
+
+    # get all genres of movie
+    all_genres = np.array([])
+    for genres in movies_df['Genres']:
+        for genre in genres.split('|'):
+            all_genres = np.append(all_genres, genre)
+    all_genres = np.unique(all_genres)
+    #print('all genres:\n', all_genres)
+
+    movies_info = np.zeros((max_movieid, all_genres.shape[0]))
+    users_info = np.zeros((max_userid, 23))
+
+    for idx, user_id in enumerate(users_df['UserID']):
+        gender = 1 if users_df['Gender'][idx] == 'M' else 0
+        occu = np.zeros(np.max(np.unique(users_df['Occupation'])) + 1)
+        occu[users_df['Occupation'][idx]] = 1
+        tmp = [gender, users_age[idx]]
+        tmp.extend(occu)
+        users_info[user_id - 1] = tmp
+
+    for idx, movie_id in enumerate(movies_df['movieID']):
+        genres = movies_df['Genres'][idx].split('|')
+        tmp = np.zeros(all_genres.shape[0])
+        for genre in genres:
+            tmp[np.where(all_genres == genre)[0][0]] = 1
+        movies_info[movie_id - 1] = tmp
+
+# ==== Model Parameter =========================================================
+embedding_dim = int(args.dimension)
+split_ratio = 0.1
+DROPOUT_RATE = 0.3
+
+# ==== Model ===================================================================
 User_input = Input(shape = (1,),dtype = 'int32')
 User_embed = Embedding(output_dim = embedding_dim,
                        input_dim = max_userid,
@@ -80,7 +118,7 @@ User_embed = Embedding(output_dim = embedding_dim,
                        embeddings_regularizer = regularizers.l2(1e-5),
                        trainable=True)(User_input)
 User_reshape = Reshape((embedding_dim,))(User_embed)
-User_reshape = Dropout(DROPOUT_RATE)(User_reshape)
+User_reshape = Dropout(0.1)(User_reshape)
 User_bias = (Embedding(output_dim = 1,
                        input_dim = max_userid,
                        input_length = 1,
@@ -97,7 +135,7 @@ Movie_embed = Embedding(output_dim = embedding_dim,
                         embeddings_regularizer = regularizers.l2(1e-5),
                         trainable=True)(Movie_input)
 Movie_reshape = Reshape((embedding_dim,))(Movie_embed)
-Movie_reshape = Dropout(DROPOUT_RATE)(Movie_reshape)
+Movie_reshape = Dropout(0.1)(Movie_reshape)
 Movie_bias = (Embedding(output_dim = 1,
                         input_dim = max_userid,
                         input_length = 1,
@@ -107,6 +145,7 @@ Movie_bias = (Embedding(output_dim = 1,
 Movie_bias = Flatten()(Movie_bias)
 
 if args.dnn:
+    print('Use dnn model without extra feature')
     concat = Concatenate()([User_reshape, Movie_reshape])
     dnn = Dense(256, activation='relu')(concat)
     dnn = Dropout(DROPOUT_RATE)(dnn)
@@ -119,10 +158,41 @@ if args.dnn:
     dnn = BatchNormalization()(dnn)
     output = Dense(1, activation='relu')(dnn)
     model = Model(inputs=[User_input, Movie_input], outputs = output)
+elif args.extra:
+    print('Use dnn model with extra feature')
+    U_info_emb = Embedding(input_dim = max_userid,
+                           output_dim = users_info.shape[1],
+                           weights=[users_info],
+                           trainable=False)(User_input)
+    U_info_emb = Flatten()(U_info_emb)
+
+    M_info_emb = Embedding(input_dim = max_movieid,
+                           output_dim = movies_info.shape[1],
+                           weights=[movies_info],
+                           trainable=False)(Movie_input)
+    M_info_emb = Flatten()(M_info_emb)
+
+    concat = Concatenate()([User_reshape, Movie_reshape, U_info_emb, M_info_emb])
+    dnn = Dense(256, activation='relu')(concat)
+    dnn = Dropout(DROPOUT_RATE)(dnn)
+    dnn = BatchNormalization()(dnn)
+    dnn = Dense(256, activation='relu')(dnn)
+    dnn = Dropout(DROPOUT_RATE)(dnn)
+    dnn = BatchNormalization()(dnn)
+    dnn = Dense(256, activation='relu')(dnn)
+    dnn = Dropout(DROPOUT_RATE)(dnn)
+    dnn = BatchNormalization()(dnn)
+    output = Dense(1, activation='relu')(dnn)
+    model = Model(inputs=[User_input, Movie_input], outputs = output)
 else:
+    print('Use matrix factorization')
     Main_dot = Dot(axes = 1)([User_reshape, Movie_reshape])
-    Main_add = Add()([Main_dot, User_bias, Movie_bias])
+    if args.bias:
+        Main_add = Add()([Main_dot, User_bias, Movie_bias])
+    else:
+        Main_add = Main_dot
     model = Model([User_input,Movie_input], Main_add)
+
 
 model.summary()
 checkpoint = ModelCheckpoint(filepath = 'bestmodel%s.hdf5'%args.model,
@@ -139,15 +209,17 @@ es = EarlyStopping(monitor='val_loss',
 
 model.compile(loss = 'mse',optimizer = 'adam')
 
+# ==== Validation Set and Train Set ============================================
 (U_train, M_train, R_train),(U_val, M_val, R_val) = split_data(users,movies,rating,split_ratio)
 model.fit([U_train,M_train],R_train,
-          batch_size = 128,
+          batch_size =128,
           epochs = 80,
           validation_data = ([U_val,M_val],R_val),
           callbacks = [checkpoint,es])
 
 model = load_model('bestmodel%s.hdf5'%args.model)
 
+# ==== Test Set ================================================================
 test_path = './data/test.csv'
 users,movies,_ = read_data(test_path,False)
 users = np.array(users).astype('int')
