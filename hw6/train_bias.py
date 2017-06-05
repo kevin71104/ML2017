da97a5fe7a3a9e6cd925eadbe0af2ba6f3487f69
kevin71104 <b03901026@ntu.edu.tw>
2017-06-05 14:38:41 +0800

    pass strong baseline better

--- hw6/train_bias.py
+++ hw6/train_bias.py
@@ -1,8 +1,10 @@
 import numpy as np
 import sys
 from keras.models import Sequential,Model, load_model
-from keras.layers import Input,Embedding, Reshape, Merge, Dropout,Flatten,Dot,Add, Flatten
+from keras.layers import Input,Embedding, Reshape, Merge, Dropout,Flatten,Dot,Add, Flatten, BatchNormalization, Concatenate, Dense
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras import regularizers
+from argparse import ArgumentParser
 
 def read_data(path,train):
     users = []
@@ -40,65 +42,90 @@ def split_data(U,M,R,split_ratio):
 
     return (U_train,M_train,R_train),(U_val,M_val,R_val)
 
-#print('arguments: %d' %len(sys.argv))
+# ==== parsing arguments =======================================================
+parser = ArgumentParser()
+parser.add_argument('--dnn', action='store_true', help='Use DNN model')
+parser.add_argument('-n', '--normal', action='store_true', help='use Normalization')
+parser.add_argument('-m', '--model', help='model name')
+parser.add_argument('-o', '--output', help='output name')
+args = parser.parse_args()
 
+# ==== Read Dataset ============================================================
 train_path = './data/train.csv'
 users,movies,rating = read_data(train_path,True)
 users = np.array(users).astype('int')
 movies = np.array(movies).astype('int')
 rating = np.array(rating).astype('float')
 
-r_mean = np.mean(rating)
-r_std = np.std(rating)
-rating = (rating - r_mean) / r_std
-
-print('rating mean(%f) and rating std(%f)'%(r_mean,r_std))
+if args.normal:
+    r_mean = np.mean(rating)
+    r_std = np.std(rating)
+    rating = (rating - r_mean) / r_std
+    print('rating mean(%f) and rating std(%f)'%(r_mean,r_std))
 
 max_userid = np.max(users)
 max_movieid = np.max(movies)
-
 print("Find maximum user id {} and maximum movie id {} in training set..."\
 	.format(max_userid,max_movieid))
 
-embedding_dim = 16
+embedding_dim = 10
 split_ratio = 0.1
+DROPOUT_RATE = 0.1
 
 User_input = Input(shape = (1,),dtype = 'int32')
 User_embed = Embedding(output_dim = embedding_dim,
                        input_dim = max_userid,
                        input_length = 1,
                        embeddings_initializer='random_normal',
+                       embeddings_regularizer = regularizers.l2(1e-5),
                        trainable=True)(User_input)
 User_reshape = Reshape((embedding_dim,))(User_embed)
+User_reshape = Dropout(DROPOUT_RATE)(User_reshape)
 User_bias = (Embedding(output_dim = 1,
                        input_dim = max_userid,
                        input_length = 1,
                        embeddings_initializer='zeros',
+                       embeddings_regularizer = regularizers.l2(1e-5),
                        trainable=True)(User_input))
 User_bias = Flatten()(User_bias)
-
 
 Movie_input = Input(shape = (1,),dtype = 'int32')
 Movie_embed = Embedding(output_dim = embedding_dim,
                         input_dim = max_movieid,
                         input_length = 1,
                         embeddings_initializer='random_normal',
+                        embeddings_regularizer = regularizers.l2(1e-5),
                         trainable=True)(Movie_input)
 Movie_reshape = Reshape((embedding_dim,))(Movie_embed)
+Movie_reshape = Dropout(DROPOUT_RATE)(Movie_reshape)
 Movie_bias = (Embedding(output_dim = 1,
                         input_dim = max_userid,
                         input_length = 1,
                         embeddings_initializer='zeros',
+                        embeddings_regularizer = regularizers.l2(1e-5),
                         trainable=True)(Movie_input))
 Movie_bias = Flatten()(Movie_bias)
 
-Main_dot = Dot(axes = 1)([User_reshape, Movie_reshape])
-Main_add = Add()([Main_dot, User_bias, Movie_bias])
+if args.dnn:
+    concat = Concatenate()([User_reshape, Movie_reshape])
+    dnn = Dense(256, activation='relu')(concat)
+    dnn = Dropout(DROPOUT_RATE)(dnn)
+    dnn = BatchNormalization()(dnn)
+    dnn = Dense(256, activation='relu')(dnn)
+    dnn = Dropout(DROPOUT_RATE)(dnn)
+    dnn = BatchNormalization()(dnn)
+    dnn = Dense(256, activation='relu')(dnn)
+    dnn = Dropout(DROPOUT_RATE)(dnn)
+    dnn = BatchNormalization()(dnn)
+    output = Dense(1, activation='relu')(dnn)
+    model = Model(inputs=[User_input, Movie_input], outputs = output)
+else:
+    Main_dot = Dot(axes = 1)([User_reshape, Movie_reshape])
+    Main_add = Add()([Main_dot, User_bias, Movie_bias])
+    model = Model([User_input,Movie_input], Main_add)
 
-model = Model([User_input,Movie_input],Main_add)
 model.summary()
-
-checkpoint = ModelCheckpoint(filepath = 'bestmodel%s.hdf5'%sys.argv[1],
+checkpoint = ModelCheckpoint(filepath = 'bestmodel%s.hdf5'%args.model,
                              verbose = 1,
                              save_best_only = True,
                              save_weights_only = False,
@@ -114,22 +141,23 @@ model.compile(loss = 'mse',optimizer = 'adam')
 
 (U_train, M_train, R_train),(U_val, M_val, R_val) = split_data(users,movies,rating,split_ratio)
 model.fit([U_train,M_train],R_train,
-          batch_size = 32,
-          epochs = 30,
+          batch_size = 128,
+          epochs = 80,
           validation_data = ([U_val,M_val],R_val),
           callbacks = [checkpoint,es])
 
-model.save('model%s.hdf5'%sys.argv[1])
-
-#model = load_model('bestmodel%s.hdf5'%sys.argv[1])
+model = load_model('bestmodel%s.hdf5'%args.model)
 
 test_path = './data/test.csv'
 users,movies,_ = read_data(test_path,False)
 users = np.array(users).astype('int')
 movies = np.array(movies).astype('int')
-output = model.predict([users,movies])
-output = output * r_std + r_mean
-with open(sys.argv[2],'w') as f:
+
+output = model.predict([users,movies], verbose=1)
+if args.normal:
+    output = output * r_std + r_mean
+
+with open(args.output,'w') as f:
 	f.write("TestDataID,Rating\n")
 	for i,rating in enumerate(output):
             if round(rating[0]) > 5:
